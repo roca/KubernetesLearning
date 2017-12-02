eb552f0fca16a6e8debd4b1abb74b9d386809e84
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2017-12-02 05:47:39 -0500

    Pushed nodejs web app to docker hub

--- udemy/kubernetes-course/index.js
+++ udemy/kubernetes-course/index.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.get('/',function(req, res) {
-   res.send('Hello Worldxxx'); 
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-
-var server = app.listen(3000,function() {
+var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app list at http://%s:%s',host,port);
+  console.log('Example app listening at http://%s:%s', host, port);
 });
