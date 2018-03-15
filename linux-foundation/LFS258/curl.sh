535587d24439e3cbff1a66baa01813ee6e692569
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2018-03-15 04:07:07 -0400

    Lab 6.3 step 1

--- linux-foundation/LFS258/curl.sh
+++ linux-foundation/LFS258/curl.sh
@@ -2,4 +2,5 @@
 
 
 #curl --cert ./client.pem --key ./client-key.pem --cacert ./ca.pem https://api.kubernetes.desertfoxdev.org/api/v1/pods
-curl --cert ./client.pem --key ./client-key.pem --cacert ./ca.pem https://api.kubernetes.desertfoxdev.org/api/v1/namespaces/default/pods -XPOST -H'Content-Type: application/json' -d@curlpod.json
+#curl --cert ./client.pem --key ./client-key.pem --cacert ./ca.pem https://api.kubernetes.desertfoxdev.org/api/v1/namespaces/default/pods -XPOST -H'Content-Type: application/json' -d@curlpod.json
+curl https://api.kubernetes.desertfoxdev.org/api/v1/namespaces --header "Authorization: Bearer $token" -k
